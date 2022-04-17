44187707699512dbf9a25c0101115e2ebcf451b9
DuFong <youngjewall@me.com>
2022-04-18 00:54:51 +0900

    First Commit

---
+++ src/main/java/com/wejbsom/batchmockinvestment/BatchMockInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.wejbsom.batchmockinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMockInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchMockInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wejbsom/batchmockinvestment/BatchMockInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wejbsom.batchmockinvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchMockInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
